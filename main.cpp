392c59282bd059f0a7a1729811e0074c232e2149
Liberess <58577796+Liberess@users.noreply.github.com>
2021-09-10 02:40:50 +0000

    Create Window & Renderer

--- main.cpp
+++ main.cpp
@@ -1,13 +1,79 @@
-#include <stdio.h>
+#include <SDL.h>
+#include <iostream>
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
 
 int main(int argc, char* args[])
 {
-  int num1, num2;
+  if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  {
+    g_pWindow = SDL_CreateWindow
+      (
+        "HelloSDL", // 실습1
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        512, // 실습1
+        512, // 실습1
+        SDL_WINDOW_SHOWN
+      );
+    
+    if(g_pWindow != 0)
+      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+  }
+  else
+  {
+    return 1;
+  }
 
-  printf("두 개의 값 입력 : ");
-  scanf("%d %d", &num1, &num2);
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255); // 실습2
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
 
-  printf("덧셈 결과 : %d\n", num1+num2);
+  SDL_Delay(2000);
+
+  SDL_DestroyWindow(g_pWindow); // 윈도우 삭제
+  SDL_DestroyRenderer(g_pRenderer); // 렌더러 삭제
+
+  SDL_Delay(2000);
+  
+  // 새로 윈도우 생성
+  g_pWindow = SDL_CreateWindow
+    (
+      "MySDL", 
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      720,
+      480,
+      SDL_WINDOW_MAXIMIZED // 창 최대화
+    );
+    
+  if(g_pWindow != 0)
+    g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+
+  // 2초 간격으로 Red->Green->Cyan 바탕색 변경
+  SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255); // 개인
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
+
+  SDL_Delay(2000);
+
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 255, 0, 255); // 개인
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
+
+  SDL_Delay(2000);
+
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 255, 255, 255); // 개인
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
+
+  SDL_Delay(2000);
+
+  SDL_DestroyWindow(g_pWindow); // 윈도우 창 삭제
+  SDL_DestroyRenderer(g_pRenderer); // 렌더러 삭제
+  
+  SDL_Quit();
 
   return 0;
 }
