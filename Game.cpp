4431832cb0a31df6299fed38468d23c689760918
Liberess <58577796+Liberess@users.noreply.github.com>
2021-09-30 03:35:06 +0000

    Create TextureManager

--- Game.cpp
+++ Game.cpp
@@ -5,8 +5,6 @@ bool Game::Init(const char *title, int xpos, int ypos,  int width, int height, i
   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
   {
     m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-    mBorderlineX = width;
-    mBorderlineY = height;
 
     if (m_pWindow != 0)
     {
@@ -17,22 +15,7 @@ bool Game::Init(const char *title, int xpos, int ypos,  int width, int height, i
       else
         return false; // 랜더러 생성 실패
       
-      SDL_Surface* pTempSurface = IMG_Load("Assets/Run (32x32).png");
-
-      if(pTempSurface != 0)
-      {
-        m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-        SDL_FreeSurface(pTempSurface);
-
-        m_sourceRectangle.w = 32;
-        m_sourceRectangle.h = 32;
-        m_destinationRectangle.w = 64;
-        m_destinationRectangle.h = 64;
-
-        SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255); //붉은색 배경
-      }
-      else
-        return false;
+      mTextureManager.Load("Assets/animate-alpha.png", "animate", m_pRenderer);
     }
     else
     {
@@ -48,36 +31,17 @@ bool Game::Init(const char *title, int xpos, int ypos,  int width, int height, i
   return true;
 }
 
-//지금은 flip X 하고 있습니다.
-
-/* void Game::StretchTextureEx(SDL_Renderer* renderer, int x, int y, int w, int h, SDL_Texture* texture, float angle, SDL_RendererFlip flip = SDL_FLIP_NONE)
-{
-  SDL_Point center;
-
-  m_sourceRectangle.x = m_sourceRectangle.y = 0;
-  SDL_QueryTexture(texture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-
-  m_destinationRectangle.x = x;
-  m_destinationRectangle.y = y;
-  m_destinationRectangle.w = w;
-  m_destinationRectangle.h = h;
-} */
-
 void Game::Update()
 {
-  if(m_destinationRectangle.x != previousPosX ||
-      m_destinationRectangle.y != previousPosY)
-    m_sourceRectangle.x = 32 * ((SDL_GetTicks() / 100) % 12);
+  mCurrentFrame = (SDL_GetTicks() / 100) % 6;
 }
 
 void Game::Render()
 {
   SDL_RenderClear(m_pRenderer);
 
-  if(direction == RIGHT)
-    SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
-  else
-    SDL_RenderCopyEx(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle, 0, 0, SDL_FLIP_HORIZONTAL);
+  mTextureManager.Draw("animate", 0, 0, 128, 82, m_pRenderer);
+  mTextureManager.DrawFrame("animate", 100, 100, 128, 82, 0, mCurrentFrame, m_pRenderer);
     
   SDL_RenderPresent(m_pRenderer);
 }
@@ -91,9 +55,6 @@ void Game::HandleEvents()
 {
   SDL_Event event;
 
-  previousPosX = m_destinationRectangle.x;
-  previousPosY = m_destinationRectangle.y;
-
   if (SDL_PollEvent(&event))
   {
     switch (event.type)
@@ -102,18 +63,18 @@ void Game::HandleEvents()
       switch(event.key.keysym.sym)
       {
       case SDLK_UP:
-        m_destinationRectangle.y -= moveSpeed;
+        //m_destinationRectangle.y -= moveSpeed;
         break;
       case SDLK_LEFT:
         direction = LEFT;
-        m_destinationRectangle.x -= moveSpeed;
+        //m_destinationRectangle.x -= moveSpeed;
         break;
       case SDLK_DOWN:
-        m_destinationRectangle.y += moveSpeed;
+        //m_destinationRectangle.y += moveSpeed;
         break;
       case SDLK_RIGHT:
         direction = RIGHT;
-        m_destinationRectangle.x += moveSpeed;
+        //m_destinationRectangle.x += moveSpeed;
         break;
       }
       break;
--- Game.h
+++ Game.h
@@ -3,6 +3,7 @@
 
 #include "SDL.h"
 #include "SDL_image.h"
+#include "TextureManager.h"
 #include <iostream>
 
 #define LEFT 0
@@ -22,7 +23,6 @@ public:
   ~Game() {}
 
   bool Init(const char *title, int xpos, int ypos,  int width, int height, int flags);
-  //void StretchTextureEx(SDL_Renderer* renderer, int x, int y, int w, int h, SDL_Texture* texture, float angle, SDL_RendererFlip flip = SDL_FLIP_NONE);
   void Render();
   void Update(); 
   bool Running();
@@ -33,15 +33,12 @@ private:
   SDL_Window* m_pWindow;
   SDL_Renderer* m_pRenderer;
   bool m_bRunning;
-  SDL_Texture* m_pTexture; // 이미지 리소스
-  SDL_Rect m_sourceRectangle; // 원본 사각형
-  SDL_Rect m_destinationRectangle; // 대상 사각형
-  int mBorderlineX;
-  int mBorderlineY;
   int moveSpeed;
-  int previousPosX;
-  int previousPosY;
   int direction;
+  // TextureManager의 인스턴스
+  TextureManager mTextureManager;
+  // 스프라이트 애니메이션 구현을 위해 선언
+  int mCurrentFrame;
 };
 
 #endif
